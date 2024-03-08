fb4784d324ae684a22af32fa08746132f8739a05
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-03-08 21:31:07 +0100

    distinct s3 error 'no key' for cases disk and storage

--- src/IO/WriteSettings.h
+++ src/IO/WriteSettings.h
@@ -23,9 +23,6 @@ struct WriteSettings
 
     bool s3_allow_parallel_part_upload = true;
 
-    /// Monitoring
-    bool for_object_storage = false; // to choose which profile events should be incremented
-
     bool operator==(const WriteSettings & other) const = default;
 };
 
